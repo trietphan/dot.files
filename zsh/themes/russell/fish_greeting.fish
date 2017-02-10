099f4597513e5723785d3a705d808b20070be3d1
Triet Phan <trietphan85@gmail.com>
2017-02-09 19:13:45 -0600

    Initial Commit

---
+++ zsh/themes/russell/fish_greeting.fish
@@ -0,0 +1,12 @@
+# Set global color styles, for example:
+#
+# function russell_error
+#   set_color -o red
+# end
+#
+# function russell_normal
+#   set_color normal
+#
+
+function fish_greeting
+end
